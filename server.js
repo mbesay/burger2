d0b9ec16dc834f89a50b9c64f05049af23ccf8d7
mbesay <mtb0920@gmail.com>
2017-07-13 20:15:19 -0400

    sad attempt at burgers hw

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+
+var PORT = process.env.PORT || 8080;
+var app = express();
+// static content   
+app.use(express.static(process.cwd() + "/public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+// POST
+app.use(methodOverride("_method"));
+// Handlebars.
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+// Importing routes
+var routes = require("./controllers/burgers_controller.js");
+app.use("/", routes);
+app.listen(PORT);
